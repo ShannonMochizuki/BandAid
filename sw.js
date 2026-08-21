785482dc3aa729fab1ba03bc0ef90c4364967c69
ShannonMochizuki <shannonmochizuki@gmail.com>
2026-08-21 23:17:41 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE = "bandaid-chord-vault-v2.2.1";
+const CACHE = "bandaid-chord-vault-v2.2.2";
 const CACHE_PREFIX = "bandaid-chord-vault-";
 const APP_SCOPE = "/BandAid/";
 
 const PRECACHE_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=2.2.1",
-  "./app.js?v=2.2.1",
-  "./manifest.webmanifest?v=2.2.1",
+  "./styles.css?v=2.2.2",
+  "./app.js?v=2.2.2",
+  "./manifest.webmanifest?v=2.2.2",
   "./icon-192.png",
   "./icon-512.png",
   "./icon-maskable-512.png"
